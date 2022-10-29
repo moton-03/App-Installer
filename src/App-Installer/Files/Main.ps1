29bad9a4a0c57b7995576b1e079d13733db14494
moton-03 <116878619+moton-03@users.noreply.github.com>
2022-10-29 13:46:28 +0900

    Add Code

---
+++ src/App-Installer/Files/Main.ps1
@@ -0,0 +1,33 @@
+﻿Clear-Host
+
+Write-Output -InputObject @"
+This program will search for and install apps.
+"@
+Pause
+Clear-Host
+
+$Search = Read-Host -Prompt ("App Title")
+Pause
+Clear-Host
+
+Write-Output -InputObject @"
+The following apps were found.
+Copy the App ID.
+"@
+winget search --query $Search
+Pause
+Clear-Host
+
+$ID = Read-Host -Prompt ("App ID")
+Pause
+Clear-Host
+
+winget install --query $ID
+Pause
+Clear-Host
+
+Write-Output -InputObject @"
+Done!
+"@
+Pause
+Clear-Host
